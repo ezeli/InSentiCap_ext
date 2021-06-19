c445d88601ea1568006df4210425d274a098a45f
ezeli <327578505@qq.com>
2021-06-19 22:43:18 +0800

    fix video sentiment detect: cat to add

--- models/video_sentiment_detector.py
+++ models/video_sentiment_detector.py
@@ -7,21 +7,40 @@ class VideoSentimentDetector(nn.Module):
         super(VideoSentimentDetector, self).__init__()
         self.sentiment_categories = sentiment_categories
 
+        self.two_d_feats_emb = nn.Sequential(
+            nn.Linear(settings['fc_feat_dim'], settings['fc_feat_dim'] // 4),
+            nn.ReLU(),
+            nn.Dropout(settings['dropout_p']),
+        )
+
+        self.three_d_feats_emb = nn.Sequential(
+            nn.Linear(settings['fc_feat_dim'], settings['fc_feat_dim'] // 4),
+            nn.ReLU(),
+            nn.Dropout(settings['dropout_p']),
+        )
+
+        self.audio_feats_emb = nn.Sequential(
+            nn.Linear(settings['audio_feat_dim'], settings['fc_feat_dim'] // 4),
+            nn.ReLU(),
+            nn.Dropout(settings['dropout_p']),
+        )
+
         self.output = nn.Sequential(
-            nn.Linear(settings['fc_feat_dim']*2 + settings['audio_feat_dim'], settings['fc_feat_dim']),
+            nn.Linear(settings['fc_feat_dim'] // 4, settings['fc_feat_dim'] // 4),
+            nn.ReLU(),
+            nn.Linear(settings['fc_feat_dim'] // 4, settings['fc_feat_dim'] // 8),
             nn.ReLU(),
             nn.Dropout(settings['dropout_p']),
-            nn.Linear(settings['fc_feat_dim'], settings['fc_feat_dim'] // 2),
-            nn.ReLU(),
-            nn.Linear(settings['fc_feat_dim'] // 2, settings['fc_feat_dim'] // 4),
-            nn.ReLU(),
-            nn.Dropout(settings['dropout_p']),
-            nn.Linear(settings['fc_feat_dim'] // 4, len(sentiment_categories)),
+            nn.Linear(settings['fc_feat_dim'] // 8, len(sentiment_categories)),
         )
 
     def forward(self, two_d_feats_tensor, three_d_feats_tensor, audio_feats_tensor):
         # [bz, fc_feat_dim],[bz, fc_feat_dim],[bz, audio_feat_dim]
-        features = torch.cat([two_d_feats_tensor, three_d_feats_tensor, audio_feats_tensor], dim=-1)
+        two_d_feats_tensor = self.two_d_feats_emb(two_d_feats_tensor)
+        three_d_feats_tensor = self.three_d_feats_emb(three_d_feats_tensor)
+        audio_feats_tensor = self.audio_feats_emb(audio_feats_tensor)
+        features = two_d_feats_tensor + three_d_feats_tensor + audio_feats_tensor
+        # features = torch.cat([two_d_feats_tensor, three_d_feats_tensor, audio_feats_tensor], dim=-1)
         return self.output(features)  # [bz, num]
 
     def sample(self, two_d_feats_tensor, three_d_feats_tensor, audio_feats_tensor):
